2a764ce3374ac5a7de82e471915c5161abfcbe05
Ethan Reed <jefmacho99@gmail.com>
2025-03-04 16:24:09 -0600

    Autocommit: cipher wip

--- DigitalLogic/cipherSub.py
+++ DigitalLogic/cipherSub.py
@@ -2,14 +2,17 @@
 OG  = 'ABCD EFGH  IJKL MNOP QRST UVWX YZ01 2345 6789'
 MAP = ' 2BQF 5WRT D8IJ 6HLC OSUV K3A0 X9YZ N1G4 ME7P'
 
-def encode(s, map=MAP, og=OG):
+def clean():
+    og = OG.replace(' ', '')
+    remap = MAP.replace(' ', '')
+
+def encode(s, remap=MAP, og=OG):
     o = ''
     for i in s:
-        o += map[og.index(i)]
+        o += remap[og.index(i)]
     return o
 
-og = og.replace(' ', '')
-remap = remap.replace(' ', '')
+
 
 print(og)
 print(map)
