6e9cb7a0822db5f523a114cc43359dbdf8a21b25
EWARS2 <jefmacho99@gmail.com>
2025-03-04 17:56:43 -0600

    Autocommit: cipher finished

--- DigitalLogic/cipherSub.py
+++ DigitalLogic/cipherSub.py
@@ -2,7 +2,6 @@
 OG = 'ABCD EFGH  IJKL MNOP QRST UVWX YZ01 2345 6789'
 REMAP = ' 2BQF 5WRT D8IJ 6HLC OSUV K3A0 X9YZ N1G4 ME7P'
 
-
 def clean(og, remap):
     og = og.replace(' ', '')
     remap = remap.replace(' ', '')
@@ -10,13 +9,11 @@ def clean(og, remap):
     print(remap)
     return og, remap, ''
 
-
 def encode(s, remap=REMAP, og=OG):
     og, remap, o = clean(og, remap)
     for i in s:
         o += remap[og.index(i)]
     return o
-
 
 def decode(s, remap=REMAP, og=OG):
     og, remap, o = clean(og, remap)
@@ -25,5 +22,6 @@ def decode(s, remap=REMAP, og=OG):
     return o
 
 
-print(encode("HELLO9"))
-print(decode("T5JJLP"))
+x = encode("HELLO9")
+print(x)
+print(decode(x))
