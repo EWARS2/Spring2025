74829e5b457a1eed3d79f84c9e1187fa0c54077a
Ethan Reed <jefmacho99@gmail.com>
2025-03-04 14:28:48 -0600

    Autocommit: 2comp

--- DigitalLogic/hexCalc.py
+++ DigitalLogic/hexCalc.py
@@ -1,27 +1,35 @@
+"""
+Two's Complement from here:
+https://stackoverflow.com/questions/16255496/format-negative-integers-in-twos-complement-representation
+"""
 import re
-def process(e, base=None):
+def process(f, base=None):
     # Format
-    e = e.capitalize()
-    e = e.replace('X', '*')
+    f = f.capitalize()
+    f = f.replace('X', '*')
 
     # Guess base if unspecified
     if base is None:
-        if re.search(r"[2-9A-F]", e):
+        if re.search(r"[2-9A-F]", f):
             base = 16
         else:
             base = 2
 
     # Generate eval expression and eval()
-    e = e.split()
-    for i in range(len(e)):
-        if re.search(r"[A-F0-9]", e[i]):
-            e[i] = "int(" + "'" + e[i] + "'" + ", base)"
-    e = eval(" ".join(e))
+    f = f.split()
+    for i in range(len(f)):
+        if re.search(r"[A-F0-9]", f[i]):
+            f[i] = "int(" + "'" + f[i] + "'" + ", base)"
+    f = eval(" ".join(f))
 
     # Output
-    print("Dec: ", e)
-    print("Hex: ", hex(e))
-    print("Bin: ", bin(e))
+    print("\nOne's Complement")
+    print("Dec: ", f)
+    print("Hex: ", hex(f))
+    print("Bin: ", bin(f))
+    print("\nTwo's Complement")
+    print("Dec: ", f)
+    print("Hex: ", hex(f % (1 << 8)))
+    print("Bin: ", bin(f % (1 << 8)))
 
-
-process('319',10)
+process('-108', 10)
