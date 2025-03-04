bc9b80b101665a49c0ea31eefeffb48756fd64e5
Ethan Reed <jefmacho99@gmail.com>
2025-03-04 12:08:14 -0600

    Autocommit: 2s complement

---
+++ DigitalLogic/twosComplement.py
@@ -0,0 +1,16 @@
+"""
+Retrieved from here:
+https://stackoverflow.com/questions/1604464/twos-complement-in-python
+"""
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val                         # return positive value as is
+
+binary_string = '11111101'
+o = twos_comp(int(binary_string,2), 8)
+# o = twos_comp(int('0xFFFFFFFF',16), 32)
+print("Dec: ", o)
+print("Hex: ", hex(o))
+print("Bin: ", bin(o))
