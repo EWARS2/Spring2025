a68a2995c88589c069f9b08e81e133032c95640a
Ethan Reed <jefmacho99@gmail.com>
2025-03-07 15:12:49 -0600

    Autocommit: Lists

--- HTML/HTMLStrip.py
+++ HTML/HTMLStrip.py
@@ -1,8 +1,4 @@
 # By Ethan Reed 2025/2/27, with help from ChatGPT for the regex.
-"""
-TODO: See if it's possible to keep links, but remove buttons,
- since they're nonfunctional and only clutter pages.
-"""
 
 import re
 import html
@@ -12,7 +8,6 @@ def html_strip(s="sample1<main>sample2</main>sample3"):
     s = s.replace("\n", "")
 
     # Cleanup links
-    # TODO: This half works
     s = re.sub(r'<a[^>]*href=["\']([^"\']+)["\'][^>]*>', r' \1 ', s, flags=re.S)
 
     # Remove buttons
@@ -20,7 +15,7 @@ def html_strip(s="sample1<main>sample2</main>sample3"):
     s = re.sub(regex, ' ', s)
 
     # Parse newlines
-    for i in ['br', 'p', 'article', 'aside', 'section', 'div', 'head', 'body', 'h1', 'h2', 'h3']:
+    for i in ['br', 'p', 'article', 'aside', 'section', 'div', 'head', 'body', 'h1', 'h2', 'h3', '/li']:
         regex = re.compile('<' + i + '.*?>')
         s = re.sub(regex, '\n', s)
 
@@ -38,22 +33,12 @@ def html_strip(s="sample1<main>sample2</main>sample3"):
 
     return s
 
-# Simple strip
-def tag_strip(s="sample1<main>sample2</main>sample3"):
-    # Strip HTML Tags
-    # s = s.replace("\n", "")
-    regex = re.compile('<.*?>', re.S)
-    return re.sub(regex, '', s)
-
 if __name__ == "__main__":
     # path = input("Filepath:")
     path = "curl-wikipedia.html"
     with open(path, "r", errors='ignore') as f:
         text = f.read()
     o = html_strip(text)
-    o2 = tag_strip(text)
     # print(text)
     with open("o.txt", "w", errors='ignore') as f:
         f.write(o)
-    with open("o2.txt", "w", errors='ignore') as f:
-        f.write(o2)
