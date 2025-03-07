7a6080c497d70a2ff6f89633c2296eec8c7c4d09
Ethan Reed <jefmacho99@gmail.com>
2025-03-07 15:00:05 -0600

    Autocommit: Output Formatting

--- HTML/HTMLStrip.py
+++ HTML/HTMLStrip.py
@@ -1,4 +1,8 @@
 # By Ethan Reed 2025/2/27, with help from ChatGPT for the regex.
+"""
+TODO: See if it's possible to keep links, but remove buttons,
+ since they're nonfunctional and only clutter pages.
+"""
 
 import re
 import html
@@ -7,20 +11,23 @@ def html_strip(s="sample1<main>sample2</main>sample3"):
     # Cleanup formatting
     s = s.replace("\n", "")
 
+    # Cleanup links
+    # TODO: This half works
+    s = re.sub(r'<a[^>]*href=["\']([^"\']+)["\'][^>]*>', r' \1 ', s, flags=re.S)
+
+    # Remove buttons
+    regex = re.compile('<button.*?>.*?</button>|<body.*?>.*?</a>', re.S)
+    s = re.sub(regex, ' ', s)
+
     # Parse newlines
-    for i in ['br', 'p', 'article', 'aside', 'section', 'div', 'head', 'body']:
+    for i in ['br', 'p', 'article', 'aside', 'section', 'div', 'head', 'body', 'h1', 'h2', 'h3']:
         regex = re.compile('<' + i + '.*?>')
         s = re.sub(regex, '\n', s)
 
     # Parse underlines
-    for i in ['hr', 'h1', 'h2', 'h3']:
+    for i in ['hr', '/h1', '/h2', '/h3']:
         regex = re.compile('<' + i + '.*?>')
         s = re.sub(regex, '\n-----\n', s)
-
-    # Parse formatting
-    for i in ['button']:
-        regex = re.compile('<' + i + '.*?>')
-        s = re.sub(regex, ' ', s)
 
     # Strip CSS & JS & HTML
     regex = re.compile('<style.*?>.*?</style>|<script.*?>.*?</script>|<.*?>', re.S)
@@ -31,12 +38,22 @@ def html_strip(s="sample1<main>sample2</main>sample3"):
 
     return s
 
+# Simple strip
+def tag_strip(s="sample1<main>sample2</main>sample3"):
+    # Strip HTML Tags
+    # s = s.replace("\n", "")
+    regex = re.compile('<.*?>', re.S)
+    return re.sub(regex, '', s)
+
 if __name__ == "__main__":
     # path = input("Filepath:")
-    path = "Wikipedia-Curl.html"
+    path = "curl-wikipedia.html"
     with open(path, "r", errors='ignore') as f:
         text = f.read()
-    text = html_strip(text)
-    print(text)
+    o = html_strip(text)
+    o2 = tag_strip(text)
+    # print(text)
     with open("o.txt", "w", errors='ignore') as f:
-        f.write(text)
+        f.write(o)
+    with open("o2.txt", "w", errors='ignore') as f:
+        f.write(o2)
