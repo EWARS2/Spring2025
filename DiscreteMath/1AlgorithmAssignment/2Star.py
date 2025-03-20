2e29c15c7b25ff908eb47e0c7c6e976856f5227f
EWARS2 <jefmacho99@gmail.com>
2025-03-19 21:50:34 -0500

    Autocommit: Star WIP

--- DiscreteMath/1AlgorithmAssignment/2Star.py
+++ DiscreteMath/1AlgorithmAssignment/2Star.py
@@ -4,3 +4,24 @@ Write a program to draw the following shape.
 Make sure you use a for loop.
 """
 
+# Based on sample code from Moodle
+import turtle
+import random
+bob = turtle.Turtle()
+bob.speed(0)
+page = turtle.Screen()
+page.bgcolor("black")
+bob.color("yellow")
+for star in range(100):
+  x = random.randint(0,100)
+  y = random.randint(0,360)
+  bob.penup()
+  bob.right(y)
+  bob.forward(x)
+  bob.pendown()
+  bob.begin_fill()
+  for side in range(5):
+    bob.forward(10)
+    bob.right(144)
+  bob.end_fill()
+
