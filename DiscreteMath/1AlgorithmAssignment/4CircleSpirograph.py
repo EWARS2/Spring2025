bc111393dde482570c3a5c1066abe1ca0c596b6e
EWARS2 <jefmacho99@gmail.com>
2025-03-19 22:41:40 -0500

    Autocommit: Spirographs

--- DiscreteMath/1AlgorithmAssignment/4CircleSpirograph.py
+++ DiscreteMath/1AlgorithmAssignment/4CircleSpirograph.py
@@ -4,3 +4,13 @@ Write a program that creates some interesting "spirograph" pattern based upon
 repeatedly printing out circles.
 Use one or more "for loops" to accomplish this.
 """
+import turtle
+
+bob = turtle.Turtle()
+bob.speed(0)
+
+for i in range(100):
+    bob.circle(100)
+    bob.right(91)
+
+turtle.done()
--- DiscreteMath/1AlgorithmAssignment/5LineSpirograph.py
+++ DiscreteMath/1AlgorithmAssignment/5LineSpirograph.py
@@ -4,3 +4,14 @@ Write a program that creates some interesting "spirograph" pattern based upon
 repeatedly printing out lines.
 Use one or more "for loops" to accomplish this.
 """
+import turtle
+
+bob = turtle.Turtle()
+bob.speed(0)
+
+bob.pendown()
+for i in range(100):
+    bob.forward(100)
+    bob.right(91)
+
+turtle.done()
