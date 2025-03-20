578c2819b22da2fed5cbc3fe3021f8b0fafc522d
EWARS2 <jefmacho99@gmail.com>
2025-03-20 14:34:29 -0500

    Autocommit: File ops in Pict2Div

--- ProgII/Pict2Div/Pict2Div.py
+++ ProgII/Pict2Div/Pict2Div.py
@@ -2,35 +2,43 @@
 # I should probably upload that project somewhere.
 
 from PIL import Image
+o = ""
 
-
-def load(path):
-    im = Image.open(path)
+def load(path_in):
+    im = Image.open(path_in)
     return im, im.load()
 
-def format_no(path, end=""):
-    im, pix = load(path)
+def pront(s, end="\n"):
+    global o
+    o += str(s + end)
+
+def format_no(path_in, end=""):
+    im, pix = load(path_in)
     for y in range(im.height):
         for x in range(im.width):
-            print(pix[x, y], end=end)
-        print()
+            pront(pix[x, y], end=end)
+        pront()
 
-def format_lobit(path, l=None):
+def format_lobit(path_in, l=None):
     if l is None:
         l = ['blue', 'yellow', 'red', 'brown']
-    im, pix = load(path)
+    im, pix = load(path_in)
     for y in range(im.height):
-        print('<div class = "div-special">')
+        pront('<div class = "div-special">')
         for x in range(im.width):
-            print(f'<div class = "{l[pix[x,y]]}"></div>')
-        print("</div>")
+            pront(f'<div class = "{l[pix[x,y]]}"></div>')
+        pront("</div>")
 
-def format_rgb(path):
-    im, pix = load(path)
+def format_rgb(path_in):
+    im, pix = load(path_in)
     for y in range(im.height):
-        print('<div class = "div-special">')
+        pront('<div class = "div-special">')
         for x in range(im.width):
-            print(f'<div style="background-color: rgb{pix[x,y]};"></div>')
-        print("</div>")
+            pront(f'<div style="background-color: rgb{pix[x,y]};"></div>')
+        pront("</div>")
 
 format_rgb("wiki_sample.png")
+
+with open("o.txt", "w") as f:
+    f.write(o)
+    f.close()
