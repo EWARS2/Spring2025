99fd3d533f554cc2651faced942240312b99e0ec
EWARS2 <jefmacho99@gmail.com>
2025-03-19 22:36:37 -0500

    Autocommit: Spiral

--- DiscreteMath/1AlgorithmAssignment/3Spiral.py
+++ DiscreteMath/1AlgorithmAssignment/3Spiral.py
@@ -1,3 +1,17 @@
 """
 Program #3: Spiral Write a program to draw the following pattern.
 """
+import turtle
+
+bob = turtle.Turtle()
+bob.speed(0)
+page = turtle.Screen()
+page.bgcolor("white")
+bob.color("blue")
+
+bob.pendown()
+for i in range(50, 1000, 2):
+    bob.forward(i)
+    bob.right(91)
+
+turtle.done()
