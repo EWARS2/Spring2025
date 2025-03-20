1fd1a472aa0e7250b9d8f94f15c8a385134c6ff4
EWARS2 <jefmacho99@gmail.com>
2025-03-20 12:12:39 -0500

    Autocommit: Pict2Div upd8

--- ProgII/Pict2Div/Pict2Div.py
+++ ProgII/Pict2Div/Pict2Div.py
@@ -1,16 +1,36 @@
 # Based on code I wrote sometime in 2021 to convert QR codes to NFOs.
 # I should probably upload that project somewhere.
 
-from PIL import Image #For opening images
-im = Image.open("markus.png")
-pix = im.load()
+from PIL import Image
 
-def insert(n):
-    l = ['blue', 'yellow', 'red', 'brown']
-    return f'<div class = "{l[n]}"></div>'
 
-for y in range(im.height):
-    print('<div class = "div-special">')
-    for x in range(im.width):
-        print(insert(pix[x,y]))
-    print("</div>")
+def load(path):
+    im = Image.open(path)
+    return im, im.load()
+
+def format_no(path, end=""):
+    im, pix = load(path)
+    for y in range(im.height):
+        for x in range(im.width):
+            print(pix[x, y], end=end)
+        print()
+
+def format_lobit(path, l=None):
+    if l is None:
+        l = ['blue', 'yellow', 'red', 'brown']
+    im, pix = load(path)
+    for y in range(im.height):
+        print('<div class = "div-special">')
+        for x in range(im.width):
+            print(f'<div class = "{l[pix[x,y]]}"></div>')
+        print("</div>")
+
+def format_rgb(path):
+    im, pix = load(path)
+    for y in range(im.height):
+        print('<div class = "div-special">')
+        for x in range(im.width):
+            print(f'<div style="background-color: rgb{pix[x,y]};"></div>')
+        print("</div>")
+
+format_rgb("wiki_sample.png")
