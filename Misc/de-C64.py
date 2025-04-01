63f16f495127694c8627d29484c67f8fb9ad4fa0
EWARS2 <jefmacho99@gmail.com>
2025-04-01 08:17:04 -0500

    Autocommit: cleanup

--- Misc/de-C64.py
+++ Misc/de-C64.py
@@ -6,26 +6,43 @@ Using to make mockups of C64 demos on other platforms.
 Mostly using stuff by GOTO80, such as Birds on Fire.
 """
 
-def remove_c64(s):
-    d = {
-        'V': 'X',
-        'N': '/',
-        'M': '\\',
-        'C': '-',
-        'D': '-',
-        'B': '|',
-    }
-
+def multi_replace(s, d):
     for i in d:
         s = s.replace(i, d[i])
-    return s.upper()
+    return s
 
+
+caps = {
+    '.': '\\',
+    'V': 'X',
+    'N': '/',
+    'M': '\\',
+    'C': '-',
+    'D': '-',
+    'B': '|',
+    'I': '/',
+    'E': '[',
+    'F': '#',
+    'T': ']',
+    'L': '.',
+    'P': '7',
+    'Z': 'J',
+}
+
+bowdown3_test = {
+    'a': ' ',
+    'c': ' ',
+    'b': ' ',
+    'A': '/',
+    'C': '/',
+    'B': '#'
+}
 
 path = "in.txt"
 with open(path, "r", errors='ignore') as f:
     text = f.read()
-    o = remove_c64(text)
+    o = multi_replace(text, bowdown3_test)
+    o = o.upper()
     print(o)
 #with open("o.txt", "w", errors='ignore') as f:
 #    f.write(o)
-
