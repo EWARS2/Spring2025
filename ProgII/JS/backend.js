b66b72d06cad83f591ab7d521cb13a82993cc894
EWARS2 <jefmacho99@gmail.com>
2025-04-23 09:04:44 -0500

    Autocommit: wip

--- ProgII/JS/backend.js
+++ ProgII/JS/backend.js
@@ -1,8 +1,8 @@
 const xhr = new XMLHttpRequest();
 
-chr.addEventListener('load', () {
+xhr.addEventListener('load', () => {
 	console.log(xhr.response);
 });
 
-xhr.open('GET', 'https://supersimplebackend.dev/');
+xhr.open('GET', 'https://supersimplebackend.dev');
 xhr.send();
