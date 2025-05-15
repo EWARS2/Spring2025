ed90d2b07a796b1fbb02afa7f07e372653d6d81e
EWARS2 <jefmacho99@gmail.com>
2025-05-15 11:34:09 -0500

    Autocommit: wip

---
+++ DiscreteMath/7Routing/schedule.py
@@ -0,0 +1,19 @@
+#WIP
+my_preference = 'ABCDEFGHIJK'
+my_project = [
+    ['A',  2, ''],
+    ['B',  4, ''],
+    ['C', 10, ''],
+    ['D', 15, ''],
+    ['E',  7, 'AB'],
+    ['F',  3, 'B'],
+    ['G',  4, 'CD'],
+    ['H',  5, 'CD'],
+    ['I', 15, 'EF'],
+    ['J',  5, 'H'],
+    ['K', 20, 'IG'],
+]
+
+
+def schedule(preference=my_preference, project=my_project):
+    pass
