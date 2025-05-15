4c2cb7e23a26a946b23664b4f7c6502224a65087
EWARS2 <jefmacho99@gmail.com>
2025-05-15 00:12:01 -0500

    Autocommit: wip

--- DiscreteMath/7Routing/tour.py
+++ DiscreteMath/7Routing/tour.py
@@ -1,5 +1,5 @@
 # WIP
-
+"""
 adjacency = [
     [0, 500, 200, 185, 205],
     [500, 0, 305, 360, 340],
@@ -7,5 +7,35 @@ adjacency = [
     [185, 360, 320, 0, 302],
     [205, 340, 165, 302, 0]
 ]
+"""
+
+my_adjacency = [
+    [0,  10, 12,  2,  30, 30],
+    [10,  0, 11,  30,  4, 30],
+    [12, 11,  0,  30, 30,  6],
+    [2,  30, 30,  0,   3,  7],
+    [30,  4, 30,  3,   0,  5],
+    [30, 30,  6,  7,   5,  0]
+]
+
+my_route = 'DABEFCD'
 
 
+
+def letter_to_num(s):
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    out = ''
+    for i in s:
+        out += str(alpha.index(i))
+    return out
+
+
+def cost(route=my_route, adjacency=my_adjacency):
+    route = letter_to_num(route)
+    cost = 0
+    for i in route:
+        print(adjacency[0][int(i)])
+
+    return cost
+
+print(cost())
