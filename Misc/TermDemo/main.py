3a3764d9a47a96becae1c4529567d09c6df72bdc
EWARS2 <jefmacho99@gmail.com>
2025-06-07 11:58:08 -0500

    Autocommit: wip

--- Misc/TermDemo/main.py
+++ Misc/TermDemo/main.py
@@ -1,26 +1,33 @@
 import os
+import random
+import keyboard
 from time import sleep
 
 
 # 25 lines by 80 characters
 
+HOME_CHAR = "\033[H"
 
 def display(directory="frames"):
-    for filename in sorted(os.listdir(directory)):
-        f = os.path.join(directory, filename)
-        with open(f, 'r') as file:
-            print(file.read(), end="\r", flush=True)
+    print(HOME_CHAR)
+    while True:
+        for filename in sorted(os.listdir(directory)):
+            f = os.path.join(directory, filename)
+            with open(f, 'r') as file:
+                print(file.read(), end=HOME_CHAR, flush=True)
 
 
 def generate():
     file = open("output.txt", "w")
-    s = ''
-    for i in range(80):
-        s += "#"
-    s += "\n"
-    for i in range(25):
+    chars=['\\', '/']
+    #chars=['#']
+    for i in range(24):
+        s = ''
+        for i in range(79):
+            s += random.choice(chars)
+        s += "\n"
         file.write(s)
     file.close()
 
-#display()
-generate()
+#generate()
+display()
